6ee8faa0e9279c134c720a358c7dc3c57b1da89b
kelvinchong6880-gif <kelvinchong6880@gmail.com>
2026-08-22 12:47:24 +0800

    feat: add jilian-cloud review and update verdict

--- src/data/brands.ts
+++ src/data/brands.ts
@@ -21,6 +21,7 @@ export const brands: Brand[] = [
   ['唯兔云', 'weitu-cloud', 'https://fast.v2yunvipaff.com/#/?code=nbBJVFQP'],
   ['光速云', 'guangsu', 'https://mdlky.gsyaff.com/#/?code=5PLKd4WN'],
   ['U1S1', 'u1s1', 'https://pkdj7.vipaff.cc/#/?code=YUCKdFlR'],
+  ['极连云', 'jilian-cloud', 'https://kdjhao.jlyvipaff.com/#/?code=3d87WSjS'],
 ].map(([name, slug, affiliateUrl], index) => ({ name, slug, affiliateUrl, rank: index + 1, affiliateLinkStatus: 'unchecked' as const, affiliateLinkCheckedAt: null }));
 
 export const getBrand = (slug: string) => brands.find((brand) => brand.slug === slug);
