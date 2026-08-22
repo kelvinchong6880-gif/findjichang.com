fbdd7f4c35fed00c062e6fb400477dcf6b70b1e0
kelvinchong6880-gif <kelvinchong6880@gmail.com>
2026-08-22 13:34:49 +0800

    feat: add quanqiu-cloud review and update verdict

--- src/data/brands.ts
+++ src/data/brands.ts
@@ -22,6 +22,7 @@ export const brands: Brand[] = [
   ['光速云', 'guangsu', 'https://mdlky.gsyaff.com/#/?code=5PLKd4WN'],
   ['U1S1', 'u1s1', 'https://pkdj7.vipaff.cc/#/?code=YUCKdFlR'],
   ['极连云', 'jilian-cloud', 'https://kdjhao.jlyvipaff.com/#/?code=3d87WSjS'],
+  ['全球云', 'quanqiu-cloud', 'https://sswdh.gcvipaff.com/#/?code=SHjBKSgm'],
 ].map(([name, slug, affiliateUrl], index) => ({ name, slug, affiliateUrl, rank: index + 1, affiliateLinkStatus: 'unchecked' as const, affiliateLinkCheckedAt: null }));
 
 export const getBrand = (slug: string) => brands.find((brand) => brand.slug === slug);
