a32e3c4d47fe9da2ea5c72672a75eaf3b443a3b8
kelvinchong6880-gif <kelvinchong6880@gmail.com>
2026-08-22 11:50:37 +0800

    feat: add muguang review and update verdict

--- src/data/brands.ts
+++ src/data/brands.ts
@@ -11,6 +11,7 @@ export const brands: Brand[] = [
   ['微风', 'weifeng', 'https://edp01.breezenetaff.com/#/?code=hM8APccJ'],
   ['SOGO云', 'sogo-yun', 'https://wzjc.sogoyunaff.cc/#/?code=BC2BL855'],
   ['飞猫云', 'feimao-yun', 'https://flycat1.flycatvipaff.cc/#/?code=w5lO9fqB'],
+  ['暮光', 'muguang', 'https://varnexa.twilightaff.com/#/?code=2ILQOoYB'],
 ].map(([name, slug, affiliateUrl], index) => ({ name, slug, affiliateUrl, rank: index + 1, affiliateLinkStatus: 'unchecked' as const, affiliateLinkCheckedAt: null }));
 
 export const getBrand = (slug: string) => brands.find((brand) => brand.slug === slug);
