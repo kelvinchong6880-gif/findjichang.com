af5867934fbd84d6df9a24792ad28623863c189d
kelvinchong6880-gif <kelvinchong6880@gmail.com>
2026-08-22 14:12:14 +0800

    feat: add muguang brand avatar image

--- src/data/brands.ts
+++ src/data/brands.ts
@@ -27,6 +27,6 @@ export const brands: Brand[] = [
   ['光年梯', 'guangnian', 'https://ggmq.gntaff.com/#/?code=hTN6UF4T'],
   ['一翻云', 'yifan', 'https://wzjc.1flyunaff.cc/#/?code=0tH3Mmch'],
   ['二猫云', 'ermao', 'https://wzjc.2maoyunaff.cc/#/?code=bvsFDmSt'],
-].map(([name, slug, affiliateUrl], index) => ({ name, slug, affiliateUrl, rank: index + 1, affiliateLinkStatus: 'unchecked' as const, affiliateLinkCheckedAt: null, avatar: ['weifeng', 'sogo-yun', 'feimao-yun'].includes(slug) ? `/images/brands/${slug}.png` : undefined }));
+].map(([name, slug, affiliateUrl], index) => ({ name, slug, affiliateUrl, rank: index + 1, affiliateLinkStatus: 'unchecked' as const, affiliateLinkCheckedAt: null, avatar: ['weifeng', 'sogo-yun', 'feimao-yun', 'muguang'].includes(slug) ? `/images/brands/${slug}.png` : undefined }));
 
 export const getBrand = (slug: string) => brands.find((brand) => brand.slug === slug);
