70854da034870448e137f2fe2fadcc3a0b0ecc09
kelvinchong6880-gif <kelvinchong6880@gmail.com>
2026-08-22 15:09:45 +0800

    feat: add FlyV review

--- src/data/brands.ts
+++ src/data/brands.ts
@@ -32,6 +32,7 @@ export const brands: Brand[] = [
   ['可信云', 'kexin-cloud', 'https://work.kosingaff.com/#/?code=k7T1sOyG'],
   ['速界', 'sujie', 'https://work.speedworldaff.cc/#/?code=wZYGdnTC'],
   ['快狸', 'kuaili', 'https://work.kuailicloud.cc/#/?code=azB6yNBW'],
+  ['飞V', 'flyv', 'https://varnexa.flyvaff.com/#/?code=XsiIuDgj'],
 ].map(([name, slug, affiliateUrl], index) => ({ name, slug, affiliateUrl, rank: index + 1, affiliateLinkStatus: 'unchecked' as const, affiliateLinkCheckedAt: null, avatar: ['weifeng', 'sogo-yun', 'feimao-yun', 'muguang', 'firefly', 'kuajie-yun', 'shanyue', 'wuyou', 'lingmao', 'xingdaomeng', 'weitu-cloud', 'guangsu', 'u1s1', 'jilian-cloud', 'quanqiu-cloud', 'guangnian', 'yifan', 'ermao'].includes(slug) ? `/images/brands/${slug}${['shanyue', 'wuyou'].includes(slug) ? '.jpg' : '.png'}` : undefined }));
 
 export const getBrand = (slug: string) => brands.find((brand) => brand.slug === slug);
