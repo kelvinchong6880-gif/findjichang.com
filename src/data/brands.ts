a8e4cb9e8f049b3c5202e0e9a480739cb227ec21
kelvinchong6880-gif <kelvinchong6880@gmail.com>
2026-08-22 12:07:56 +0800

    feat: add kuajie-yun review and update verdict

--- src/data/brands.ts
+++ src/data/brands.ts
@@ -13,6 +13,7 @@ export const brands: Brand[] = [
   ['飞猫云', 'feimao-yun', 'https://flycat1.flycatvipaff.cc/#/?code=w5lO9fqB'],
   ['暮光', 'muguang', 'https://varnexa.twilightaff.com/#/?code=2ILQOoYB'],
   ['Firefly', 'firefly', 'https://vip02.fireflyaff.com/#/?code=8nDg6OEY'],
+  ['跨界云', 'kuajie-yun', 'https://vip02.kuajieaff.com/#/?code=hh3QezsW'],
 ].map(([name, slug, affiliateUrl], index) => ({ name, slug, affiliateUrl, rank: index + 1, affiliateLinkStatus: 'unchecked' as const, affiliateLinkCheckedAt: null }));
 
 export const getBrand = (slug: string) => brands.find((brand) => brand.slug === slug);
